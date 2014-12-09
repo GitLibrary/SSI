04e20ebc1bfc4657bbd946ff851d829af7bd2753
Git All <www.dharamsengar@gmail.com>
2014-12-09 07:27:21 +0530

    reference added
    more details

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
+	// Set a length limit for string which is going to be sequencially incremented
 	define('ansiLimit', 30);
+
+	// link ANSI class with your code
 	require_once('ansi.php');
 
-	$test = array('ZZZ','   1000','DASD231DASDA12Z','000000000000000','fdsad3!###','zzzzzzzzzzzzzzzz','edwaeqweq31321eae2131ea19');
+	// Different set of values for testing
+	$test = array(
+			'ZZZ','   1000','DASD231DASDA12Z','000000000000000','zzzzzzzzzzzzzzzz','edwaeqweq31321eae2131ea19',
+			'rweQW324eq321CDG','fdsad3!###'
+			);
 
+	// Creating an object
 	$ansi = new ANSI();
 
 	foreach ($test as $v) {
