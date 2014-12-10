ef1449c5effd688c5bd7168f30fa7cc7bbb10646
Git All <www.dharamsengar@gmail.com>
2014-12-10 17:15:27 +0530

    added

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once('ansi.php');
+	require_once('stringsequence.php');
 
-// values for testing
-$test = array(
-	'ZZZ','   1000','DASD231DASDA12Z','000000000000000','zzzzzzzzzzzzzzzz','edwaeqweq31321eae2131ea19',
-	'rweQW324eq321CDG','fdsad3!###'
-	);
+	// values for testing
+	$test = array(
+			'ZZZ','   1000','DASD231DASDA12Z','000000000000000','zzzzzzzzzzzzzzzz','edwaeqweq31321eae2131ea19',
+			'rweQW324eq321CDG','fdsad3!###'
+			);
+
+	foreach ($test as $v) {
+	    echo '<pre>';
+	    echo 'Old String - '.$v. '<br/>New String - '.StringSequence::Incrementor($v). '<br/><br/>';
+	    echo '</pre>';
+	}
 
 
-foreach ($test as $v) {
-    echo '<pre>';
-    echo 'Old String - '.$v. '<br/>New String - '.StringSequence::increment($v). '<br/><br/>';
-    echo '</pre>';
-}
-
-
+?>
