d8a3f952a62edf7b7a457a2d4bbb21cf6ff2fea8
Git All <www.dharamsengar@gmail.com>
2014-12-11 09:02:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	// values for testing
 	$test = array(
 			'ZZZ','   1000','DASD231DASDA12Z','000000000000000','zzzzzzzzzzzzzzzz','edwaeqweq31321eae2131ea19',
-			'rweQW324eq321CDG'
+			'rweQW324eq321CDG', 'fdsad3!###'
 			);
 
 	foreach ($test as $v) {
